7ddb5d78d571ea7e7cae82ac1f999b84a39515db
Tristan Brown <tgbrown@stanford.edu>
2018-08-12 19:07:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]+\w/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z].+[\.!?]$/) then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+if phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/)
+  return true
+else
+  return false
+end
 end
