3e3934ebb456dcbc11fef1154946b1755fbe2af0
Tristan Brown <tgbrown@stanford.edu>
2018-08-12 18:11:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]+\w/) then
+  if word.match(/^[aeiouAEIOU]+\w/)
     return true
   else
     return false
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-text.match(/\w{5))
+text.match(/\w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
